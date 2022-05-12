bd490995bfd9ff310af03748e8cbdc4c7c38e4fe
RojiTech Global <rojitech9@gmail.com>
2022-05-12 14:04:12 +0100

    init2

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// components
+import Home from "./components/home/Home";
+import NavBar from "./components/nav/NavBar";
+import Footer from "./components/footer/Footer";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Home />
+      <Footer />
+    </>
+  );
+}
+export default App;
